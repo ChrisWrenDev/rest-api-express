2462246aadc9bba9a93a7dc8225c533cf98ee4b2
Chris Wren <chris.wren@bt.com>
2024-12-15 11:58:13 +0000

    Setup logger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/conntect";
+import logger from "./utils/logger";
 
 const port = config.get<number>("port");
 
 const app = express();
 
 app.listen(port, async () => {
-  console.log("Listening on port", port);
+  logger.info("Listening on port", port);
 
   await connect();
 });
--- src/utils/conntect.ts
+++ src/utils/conntect.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import config from "config";
+import logger from "./logger";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
 
   try {
     await mongoose.connect(dbUri);
-    console.log("Conntected to db");
+    logger.info("Conntected to db");
   } catch (err) {
-    console.error("Could not conntect to db:", err);
+    logger.error("Could not conntect to db:", err);
     process.exit(1);
   }
 }
